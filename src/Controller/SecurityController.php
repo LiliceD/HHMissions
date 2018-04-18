1da11ae51c29a9066a1c9bc88339dd2245c6947e
Liliced <lilice.dhn@gmail.com>
2018-04-18 17:52:20 +0200

    redirect homepage and login when connected

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends Controller
@@ -12,8 +13,13 @@ class SecurityController extends Controller
     /**
      * @Route("/login", name="login")
      */
-    public function login(Request $request, AuthenticationUtils $authenticationUtils)
+    public function login(Request $request, AuthenticationUtils $authenticationUtils, AuthorizationCheckerInterface $authChecker)
     {
+    	if ($authChecker->isGranted('IS_AUTHENTICATED_FULLY')) {
+	        // If user is connected, redirect to mission list
+            return $this->redirectToRoute('app_mission_list');
+	    }
+
     	// get the login error if there is one
 	    $error = $authenticationUtils->getLastAuthenticationError();
 
