0b6105e64d4df33a08b04155bc06598f583c2878
LiliceD <lilice.dhn@gmail.com>
2018-12-14 04:16:10 +0100

    Rafactor Address JS

--- public/js/functions.js
+++ public/js/functions.js
@@ -57,6 +57,8 @@ function addMissionsSorting(tables) {
     });
 }
 
+
+
 /**
  * Show number of GLA / Volunteer / Address options selected
  */
@@ -82,90 +84,7 @@ function updateNumberOfCheckedOptionsInBadge() {
         $('span.badge', div).text(numberChecked);
     });
 }
-// Add sorting to given table to all columns but the last n ones
-function sortTable(table, n = 0) {
-    let tableHeadRows = table.querySelectorAll('thead > tr > th');
-      
-    for (let i = 0, c = tableHeadRows.length - n; i < c; i++) {
-        tableHeadRows[i].addEventListener('click', () => {
-          sortTableColumn(table, i);
-        });
-    }
-}
 
-// Used in sortTable
-// Add sorting to given table on click on given column n
-function sortTableColumn(table, n) {
-    let rows, switching = true, i, x, y, shouldSwitch, dir, switchcount = 0;
-
-    // Set the sorting direction to ascending:
-    dir = 'asc'; 
-    
-    /* Make a loop that will continue until
-    no switching has been done: */
-    while (switching) {
-        // Start by saying: no switching is done:
-        switching = false;
-
-        rows = table.getElementsByTagName('tr');
-
-        /* Loop through all table rows (except the
-        first, which contains table headers): */
-        for (i = 1, c = rows.length - 1; i < c; i++) {
-            // Start by saying there should be no switching:
-            shouldSwitch = false;
-
-            /* Get the two elements you want to compare,
-            one from current row and one from the next: */
-            x = rows[i].getElementsByTagName('td')[n];
-            y = rows[i + 1].getElementsByTagName('td')[n];
-
-            if (x.getAttribute('data-is-date')) {
-                xSplit = x.innerHTML.split('/');
-                ySplit = y.innerHTML.split('/');
-
-                xVal = xSplit[2] + xSplit[1] + xSplit[0];
-                yVal = ySplit[2] + ySplit[1] + ySplit[0];
-            } else {
-                xVal = x.innerHTML.toLowerCase();
-                yVal = y.innerHTML.toLowerCase();
-            }
-
-            /* Check if the two rows should switch place,
-            based on the direction, asc or desc: */
-            if (dir === 'asc') {
-                if (xVal > yVal) {
-                    // If so, mark as a switch and break the loop:
-                    shouldSwitch = true;
-                    break;
-                }
-            } else if (dir === 'desc') {
-                if (xVal < yVal)  {
-                    // If so, mark as a switch and break the loop:
-                    shouldSwitch = true;
-                    break;
-                }
-            }
-        }
-
-        if (shouldSwitch) {
-            /* If a switch has been marked, make the switch
-            and mark that a switch has been done: */
-            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-            switching = true;
-
-            // Each time a switch is done, increase this count by 1:
-            switchcount ++; 
-        } else {
-            /* If no switching has been done AND the direction is "asc",
-            set the direction to "desc" and run the while loop again. */
-            if (switchcount === 0 && dir === 'asc') {
-                dir = 'desc';
-                switching = true;
-            }
-        }
-    }
-}
 
 /**
  * -- FILL TABLE OF MISSIONS --
